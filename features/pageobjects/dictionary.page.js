8ae25d5cbc0ded00491dd1dbddd6c2f5274e43cc
Bimo <muhammad.brahmantyo@gmail.com>
2024-12-19 22:07:31 +0700

    added id for dictionary

--- features/pageobjects/dictionary.page.js
+++ features/pageobjects/dictionary.page.js
@@ -10,6 +10,30 @@ class DictionaryPage{
     get btnHistory() {
         return $('//android.view.View[@content-desc="History"]')
     }
+
+    get lblPlaceholderDictionary(){
+        return $('//android.widget.TextView[@content-desc="lblPlaceholderDictionary"]')
+    }
+
+    get btnPaste(){
+        return $('//android.view.View[@content-desc="btnPaste"]')
+    }
+
+    get txtInputDictionary(){
+        return $('//android.view.View[@content-desc="txtInputDictionary"]')
+    }
+
+    get btnTutur(){
+        return $('//android.view.View[@content-desc="btnTutur"]')
+    }
+
+    get lblDictionaryResult(){
+        return $('//android.widget.TextView[@content-desc="lblDictionaryResult"]')
+    }
+
+    get btnNextDictionary(){
+        return $('//android.view.View[@content-desc="btnNextDictionary"]')
+    }
 }
 
 export default new DictionaryPage();
